db19de3e3de7a93d5429f5bfff9b5bbf6ad989f0
yungdenzel <oyomafia@gmail.com>
2019-11-07 11:39:40 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header'
-import './App.css';
+import './styles/App.css';
 import DeviceSection from './DeviceSection';
 import {device} from './data/deviceData';
 import './styles/App.css';
