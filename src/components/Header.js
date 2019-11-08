99166e27e84c2fd8400da4c4b85256c45e63a8aa
yungdenzel <oyomafia@gmail.com>
2019-11-08 16:10:52 +0100

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ const Header = () => {
         return  (
 
          <div>
+             <div className="container-fluid">
              <section className="header">
             <nav className="navbar navbar-expand-lg  bg-white">
                 <a className="navbar-brand netflix" href="/">NETFLIX</a>
@@ -30,12 +31,12 @@ const Header = () => {
                             <source src="https://res.cloudinary.com/yungdenzel/video/upload/v1572699511/youtube_nnmhsd.mp4" type="video/mp4" />
             />            
             </video>   
-                        <h1 className="text-white mar"> Connect to Netflix using <br></br>your favourite<br></br>devices</h1>          
+                        <h1 className="text-white text-center col-xs-8 mar"> Connect to Netflix using <br></br>your favourite devices</h1>          
         </div>
            </section>
            
             </div>
-         
+         </div>
 
     )
     }
