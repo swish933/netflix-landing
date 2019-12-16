8166b25a04f5c0813365ea25a67cc45b9a3eff2e
Ikem Nomso <ikemnomso@gmail.com>
2019-12-16 12:14:59 +0100

    file restructure

--- src/components/Device.js
+++ src/components/Device.js
@@ -1,41 +1,35 @@
-import React from "react";
-import "../styles/Device.css";
+import React from 'react';
+import './styles/Device.css'
 
 class Device extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.props.onDeviceClick(this.props.id);
-  }
-
-  render() {
-    let activeIndex = this.props.activeIndex;
-    let deviceIndex = this.props.id;
-    let img;
-    if (activeIndex == deviceIndex) {
-      img = this.props.activeimg;
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);  
     }
-    img = this.props.img;
 
-    return (
-      <div
-        className={
-          activeIndex == deviceIndex ? "deviceCard-active" : "deviceCard"
+    handleClick() {
+        this.props.onDeviceClick(this.props.id);
+    }
+
+    render() {
+        let activeIndex = this.props.activeIndex;
+        let deviceIndex = this.props.id;
+        let img = this.props.img;
+        if (activeIndex == deviceIndex) {
+            img = this.props.activeimg;
         }
-        onClick={this.handleClick.bind(this, this.deviceIndex)}
-      >
-        <div className="deviceImg pa1">
-          <img src={img} alt="deviceImg" />
-        </div>
-        <div className="deviceName">
-          <p>{this.props.name}</p>
-        </div>
-      </div>
-    );
-  }
+        return (
+                <div className= {(activeIndex == deviceIndex) ? 'deviceCard-active':'deviceCard'} onClick={this.handleClick.bind(this, this.deviceIndex)} >
+                    <div className='deviceImg pa1'>
+                        <img src={img} alt='deviceImg'/>
+                    </div>
+                    <div className='deviceName'>
+                        <p>{this.props.name}</p>
+                    </div>
+                </div>
+        )
+    }
 }
+
 
 export default Device;
---
+++ src/components/Welcome.js
@@ -0,0 +1,24 @@
+import React from "react";
+import classes from "../styles/BackgroundVideo.module.css";
+
+const videoSource =
+  "https://res.cloudinary.com/yungdenzel/video/upload/v1572699511/youtube_nnmhsd.mp4";
+
+const Welcome = () => (
+  <div>
+    <div className={classes.Container}>
+      <video autoPlay="autoplay" loop="loop" muted className={classes.Video}>
+        <source src={videoSource} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <div className={classes.Content}>
+        <h1 className="fw9 f1-l f2 HelveticaNeue tc pa6-l">
+          Connect to Netflix using your favorite devices.{" "}
+        </h1>
+      </div>
+    </div>
+  </div>
+);
+
+export default Welcome;
