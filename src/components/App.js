17701056753355af493c995b0647d2e1f26ba53c
Ikem Nomso <ikemnomso@gmail.com>
2019-12-16 11:16:49 +0100

    file restructure

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Welcome from "./Welcome";
+
+import DeviceSection from "../DeviceSection";
+import { device } from "../data/deviceData";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        <Header />
+        <Welcome />
+        <DeviceSection device={device} />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/Divider.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "../styles/Device.css";
+
+export const Divider = () => (
+  <div className="divider"></div>
+);
