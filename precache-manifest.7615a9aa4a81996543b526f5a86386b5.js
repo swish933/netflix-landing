6a69eb89d731dda652671f8d4ffe3dc3e4083d28
Ikem Nomso <ikemnomso@gmail.com>
2020-01-19 23:22:08 +0100

    Updates

---
+++ precache-manifest.7615a9aa4a81996543b526f5a86386b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3891fe6284baca6a6d875af422bb2a",
+    "url": "/netflix-landing/index.html"
+  },
+  {
+    "revision": "cb7c26fa2646b4234626",
+    "url": "/netflix-landing/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a065e219bdd1924309bb",
+    "url": "/netflix-landing/static/css/main.20e826b7.chunk.css"
+  },
+  {
+    "revision": "cb7c26fa2646b4234626",
+    "url": "/netflix-landing/static/js/2.1a4c0a72.chunk.js"
+  },
+  {
+    "revision": "a065e219bdd1924309bb",
+    "url": "/netflix-landing/static/js/main.c92b323a.chunk.js"
+  },
+  {
+    "revision": "aa68af8802da82b6ab7f",
+    "url": "/netflix-landing/static/js/runtime-main.33168fec.js"
+  }
+]);
